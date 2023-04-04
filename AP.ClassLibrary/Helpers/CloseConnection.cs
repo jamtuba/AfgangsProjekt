1a10bbfed5d2e8433977c9a6952ae8acb9405819
Janus Mogensen <jamtuba@gmail.com>
2023-04-04 12:06:37 +0200

    Adding a classlibrary for common methods and models

---
+++ AP.ClassLibrary/Helpers/CloseConnection.cs
@@ -0,0 +1,12 @@
+﻿using RabbitMQ.Client;
+
+namespace AP.ClassLibrary.Helpers;
+
+public static class CloseConnection
+{
+    public static void CloseAll(IModel channel, IConnection connection)
+    {
+        channel.Close();
+        connection.Close();
+    }
+}
---
+++ AP.ClassLibrary/Helpers/GetConnection.cs
@@ -0,0 +1,15 @@
+﻿using RabbitMQ.Client;
+
+namespace AP.ClassLibrary.Helpers;
+
+public static class GetConnection
+{
+    public static IConnection ConnectionGetter(string uri)
+    {
+        var factory = new ConnectionFactory { Uri = new Uri(uri) };
+
+        var connection = factory.CreateConnection();
+
+        return connection;
+    }
+}
