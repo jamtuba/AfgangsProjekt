e661090a84b57bb3e5c59549a42237fa00ef2574
Janus Mogensen <jamtuba@gmail.com>
2023-04-21 10:19:23 +0200

    Removing the Azure.SignalR dependency

--- AP.API/Program.cs
+++ AP.API/Program.cs
@@ -24,8 +24,8 @@ builder.Configuration
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddSignalR()
-    .AddAzureSignalR();
+builder.Services.AddSignalR();
+    //.AddAzureSignalR();
 
 // options =>
 //{
@@ -82,4 +82,28 @@ app.UseCors(builder => builder
 
 app.MapHub<SignalRHub>(SignalRHub.HubUrl);
 
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateTime.Now.AddDays(index),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast");
+
 app.Run();
+
+internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
