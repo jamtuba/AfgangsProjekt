ebe5b95d6818b45f622fdb9b7b134045e49e0858
Janus Mogensen <jamtuba@gmail.com>
2023-05-07 11:10:35 +0200

    Added Cors Updated the different packages in Github Actions

--- AP.API/Program.cs
+++ AP.API/Program.cs
@@ -13,6 +13,9 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddSignalR();
 
 
+builder.Services.AddCors();
+
+
 builder.Services.AddSingleton<IRabbitMQService, RabbitMQService>();
 builder.Services.AddSingleton<IRabbitMQConsumer, RabbitMQConsumer>();
 builder.Services.AddHostedService<ConsumerHostedService>();
@@ -28,30 +31,12 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseCors(builder => builder
+    .AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+    );
+
 app.MapHub<SignalRHub>(SignalRHub.HubUrl);
 
-//var summaries = new[]
-//{
-//    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Janussy"
-//};
-
-//app.MapGet("/weatherforecast", () =>
-//{
-//    var forecast = Enumerable.Range(1, 5).Select(index =>
-//        new WeatherForecast
-//        (
-//            DateTime.Now.AddDays(index),
-//            Random.Shared.Next(-20, 55),
-//            summaries[Random.Shared.Next(summaries.Length)]
-//        ))
-//        .ToArray();
-//    return forecast;
-//})
-//.WithName("GetWeatherForecast");
-
 app.Run();
-
-//internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
-//{
-//    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-//}
